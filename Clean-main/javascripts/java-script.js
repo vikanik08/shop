e40e6769881ff958d1e6cf2afdf2e632272e2a9d
vikanik08 <lunyco@mail.ru>
2025-04-17 17:12:41 +0300

    Initial commit

---
+++ Clean-main/javascripts/java-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+   
+    
+    window.addEventListener("scroll", () => {
+        const scrollY = window.scrollY;
+       console.log (scrollY)
+    });
+});
